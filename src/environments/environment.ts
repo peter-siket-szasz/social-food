5ef19f0cfd031e395e8ccc007f48410c160e8f35
Péter Siket-Szász <peter.siket-szasz@aalto.fi>
2019-11-11 10:00:40 +0200

    Added google api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googlemaps_key: 'AIzaSyCIQEWkb1E4BEzkb_DLOSRoGkPkcOVMQxw'
 };
 
 /*
