d696e1a4ad9e117c6e3cc6511aef1a63034fa0cf
Péter Siket-Szász <peter.siket-szasz@aalto.fi>
2019-11-11 09:02:51 +0200

    Added front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './component/front-page/front-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule
