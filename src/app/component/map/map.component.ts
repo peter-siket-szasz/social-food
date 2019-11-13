39f2dbaa19d211eb823fab08e2028891ea48e092
Péter Siket-Szász <peter.siketszasz@gmail.com>
2019-11-13 14:16:27 +0200

    Added markers and info

--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleMap, MapMarker, MapInfoWindow } from "@angular/google-maps";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +7,7 @@ import { GoogleMap, MapMarker, MapInfoWindow } from "@angular/google-maps";
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  @ViewChild(MapInfoWindow, {static: false}) infoWindow: MapInfoWindow;
   width = document.getElementById("wrapper").offsetWidth - 30 + 'px';
   height = window.innerHeight - 200 + 'px';
   zoom = 15;
@@ -17,17 +18,50 @@ export class MapComponent implements OnInit {
 
   options: google.maps.MapOptions = {
     mapTypeId: 'roadmap',
-    zoomControl: false,
-    scrollwheel: false,
     disableDoubleClickZoom: false,
     maxZoom: 18,
     minZoom: 14
   }
-
+  markers = [];
+  infoContent = '';
+  dibsed = false;
 
   constructor() { }
-
+  
   ngOnInit() {
+  }
+  
+  addMarker(event) {
+    this.markers.push({
+      position: {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng()
+      },
+      label: {
+        color: 'black',
+        text: 'Tikka masala ' + (this.markers.length + 1)
+      },
+      title: 'Title ' + (this.markers.length + 1),
+      info: 'Mm, hyvää',
+      options: {
+        animation: google.maps.Animation.DROP
+      }
+    })
+  }
+
+  openInfo(marker: MapMarker, content) {
+    this.dibsed = false;
+    this.infoContent = content;
+    this.infoWindow.open(marker);
+  }
+
+  dibs() {
+    this.dibsed = true;
+    this.infoContent = 'Dibsed';
+  }
+
+  infoClosed() {
+    this.dibsed = false;
   }
 
 }
